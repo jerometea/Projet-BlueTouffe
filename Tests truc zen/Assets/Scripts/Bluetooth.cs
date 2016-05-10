cf892a5acd853badd71988a1d15fa7a6af37faea
Scarsnik <leo.stefani@wanadoo.fr>
2016-05-10 15:46:45 +0200

    Start of menu and Bluetooth connection

---
+++ Tests truc zen/Assets/Scripts/Bluetooth.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// Used to use the Bluetooth of an android appareil.
+/// </summary>
+
+public class Bluetooth {
+
+	
+}
---
+++ Tests truc zen/Assets/Scripts/EcranAccueil.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EcranAccueil : MonoBehaviour {
+    int i = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (i < 130)
+        {
+            transform.localScale += new Vector3(1, 1, 0);
+            i++;
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+	}
+}
