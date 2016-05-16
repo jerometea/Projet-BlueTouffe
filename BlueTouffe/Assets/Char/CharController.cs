0e9cc210ef9b5c516d2fe41b68b5e93b4b36271c
Duval <daubihtmania@gmail.com>
2016-05-16 18:20:43 +0200

    Add Char on map with collides

--- BlueTouffe/Assets/Char/CharController.cs
+++ BlueTouffe/Assets/Char/CharController.cs
@@ -6,13 +6,13 @@ public class CharController : MonoBehaviour {
     public Animator _anim;
     public GameObject _cam;
     public int _distCam = 5;
-    public float _speed = 1f;
+    float _cross = 1f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         _anim = GetComponent<Animator>();
         _cam = GameObject.Find( "Camera" );
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,23 +21,24 @@ public class CharController : MonoBehaviour {
 
         _anim.SetFloat( "speedY", y );
         _anim.SetFloat( "speedX", x );
+        if( x != 0 ) _cross = 0.5f;
 
         if( y > 0)
         {
-            transform.Translate( 0, y * _speed * Time.deltaTime, 0 );
+            transform.Translate( 0, (y * Time.deltaTime) * _cross, 0 );
         } else if (y < 0)
         {
-            transform.Translate( 0, y * _speed * Time.deltaTime, 0 );
+            transform.Translate( 0, (y * Time.deltaTime) * _cross, 0 );
         }
 
         if( x > 0 )
         {
-            transform.Translate( -x * _speed * Time.deltaTime, 0, 0 );
+            transform.Translate( -x * Time.deltaTime, 0, 0 );
             transform.eulerAngles = new Vector2( 0, 180 );
         }
         else if( x < 0 )
         {
-            transform.Translate( x * _speed * Time.deltaTime, 0, 0 );
+            transform.Translate( x * Time.deltaTime, 0, 0 );
             transform.eulerAngles = new Vector2( 0, 0 );
         }
 
